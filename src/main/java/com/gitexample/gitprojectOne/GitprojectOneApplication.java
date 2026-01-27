5901bf102590911563a6854f3cd9461023a25b1f
Rajendra Naik <rajanaik981@gmail.com>
2026-01-28 00:14:22 +0530

    firstOne

---
+++ src/main/java/com/gitexample/gitprojectOne/GitprojectOneApplication.java
@@ -0,0 +1,13 @@
+package com.gitexample.gitprojectOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitprojectOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitexample/gitprojectOne/GitprojectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitexample.gitprojectOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
