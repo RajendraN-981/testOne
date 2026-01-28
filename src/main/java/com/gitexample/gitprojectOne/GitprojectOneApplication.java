9ac7a13ff884ea3edc80b5d37a040aaadfb9cd2e
RajendraN-981 <rajanaik981@gmail.com>
2026-01-28 16:33:02 +0530

    Update GitprojectOneApplication.java
    "Conflict Commit"

--- src/main/java/com/gitexample/gitprojectOne/GitprojectOneApplication.java
+++ src/main/java/com/gitexample/gitprojectOne/GitprojectOneApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitprojectOneApplication {
-
+	String name="mike";
+	int x=100;
+	boolean isPresent =false;
+	int y=200;
+	double z=10.3;
+	boolean isPresent =false;
+	float m = 10.3f;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GitprojectOneApplication.class, args);
 	}
