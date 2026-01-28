a6e727f770b29bb6ea16a4527d275250b9577be7
Rajendra Naik <rajanaik981@gmail.com>
2026-01-28 22:39:15 +0530

    stashed line

--- src/main/java/com/gitexample/gitprojectOne/A.java
+++ src/main/java/com/gitexample/gitprojectOne/A.java
@@ -6,5 +6,9 @@ public class A {
         System.out.println("Iam Rajendra");
         //Welcome
         //Rajendra
+        //Some login work with OTP
+        //75%
+        //hot fix is important stop your progress and work on hot fix
+        //git stash 
     }
 }
